db4dccbd4d1953200ad374ee83989a1d4be615db
TurkiKAlqou <turkikalqou@gmail.com>
2024-07-22 16:47:16 +0300

    Created Models and Migrations
    + Models.py
    + Migrations
    # Configured Admin panel

--- ecommerce/ecomm/admin.py
+++ ecommerce/ecomm/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, Product, Category, Review, OrderItem, User, Payment
+
 
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(OrderItem)
+admin.site.register(User)
+admin.site.register(Payment)

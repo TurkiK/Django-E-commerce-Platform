3f43dee2c5c40dc7b4cb2559b668502de3afd130
TurkiKAlqou <turkikalqou@gmail.com>
2024-07-24 17:05:53 +0300

    Fixed routing issues

--- OnlineShopping/myshop/shop/urls.py
+++ OnlineShopping/myshop/shop/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('add_to_cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:pk>/', views.remove_from_cart, name='remove_from_cart'),
--- OnlineShopping/myshop/shop/views.py
+++ OnlineShopping/myshop/shop/views.py
@@ -40,7 +40,7 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return redirect('home')
+    return redirect('/')
 
 
 @login_required
